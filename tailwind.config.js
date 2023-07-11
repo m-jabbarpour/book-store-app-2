e72bc4b1fc7c9d18a213f1b3da0a2c2ff98718c0
Mortaza Jabbarpour <mortaza.jabbarpour@gmail.com>
2023-07-11 07:45:14 +0330

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,22 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#28C5CC",
+        modal: "#00000077",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
+    },
   },
   plugins: [],
 };
-
